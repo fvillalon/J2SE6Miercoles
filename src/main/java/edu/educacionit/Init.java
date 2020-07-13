53f0c1905d632fcd578de7c6b3159d37d275587f
andy <arteysoft@gmail.com>
2020-07-13 11:52:45 -0300

    ejercicio

--- src/main/java/edu/educacionit/Init.java
+++ src/main/java/edu/educacionit/Init.java
@@ -1,6 +1,5 @@
 package edu.educacionit;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,10 +8,10 @@ public class Init {
 		System.out.println("Bienvenidos a clase 2");
       		
                 Conexion[] arrCon = new Conexion[] {
-                    new Conexion(),
-                    new Conexion(),
-                    new Conexion(),
-                    new Conexion()
+                    new ConexionMYSQL(),
+                    new conexionOracle(),
+                    new ConexionMYSQL(),
+                    new conexionOracle()
                 };
                 
                 for (Conexion c : arrCon) {
@@ -21,8 +20,20 @@ public class Init {
 	}
 }
 
-class Conexion {
+interface Conexion {
+    void conectar();
+}
+
+class ConexionMYSQL implements Conexion {
     private String datosConexion = "Base de datos MySQL";
+    public void conectar() {
+        System.out.print("Conectando a: ");
+        System.out.println(datosConexion);
+    }
+}
+
+class conexionOracle implements Conexion {
+    private String datosConexion = "Base de datos ORACLE";
     public void conectar() {
         System.out.print("Conectando a: ");
         System.out.println(datosConexion);
