0885dbb439ab4c7112c9f13c7d3f8f26c5ab2aab
andy <arteysoft@gmail.com>
2020-07-13 12:53:18 -0300

    clase 3

--- src/main/java/edu/educacionit/Init.java
+++ src/main/java/edu/educacionit/Init.java
@@ -5,17 +5,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Init {
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos a clase 2");
+                System.out.println("Bienvenidos a clase 2");
       		
                 Conexion[] arrCon = new Conexion[] {
                     new ConexionMYSQL(),
                     new conexionOracle(),
-                    new ConexionMYSQL(),
+                    new ConexionMongoDb(),
                     new conexionOracle()
                 };
                 
                 for (Conexion c : arrCon) {
                     c.conectar();
+                }
+                
+                // scope - las variables definidas
+                // dentro de este scope, no van a colisionar
+                // con las variables que esten en otro socpe
+                {
+                    int[] arrEnteros = new int[args.length];
+                    
+                    for (int i = 0; i < args.length; i++) {
+                        arrEnteros[i] = Integer.parseInt(args[i]);
+                    }
+                    
+                    System.out.println("");
+                    for (int x : arrEnteros) {
+                        System.out.print("Argumento: ");
+                        System.out.println(x);
+                    }
                 }
 	}
 }
@@ -34,6 +51,14 @@ class ConexionMYSQL implements Conexion {
 
 class conexionOracle implements Conexion {
     private String datosConexion = "Base de datos ORACLE";
+    public void conectar() {
+        System.out.print("Conectando a: ");
+        System.out.println(datosConexion);
+    }
+}
+
+class ConexionMongoDb implements Conexion {
+    private String datosConexion = "Base de datos MongoDB";
     public void conectar() {
         System.out.print("Conectando a: ");
         System.out.println(datosConexion);
