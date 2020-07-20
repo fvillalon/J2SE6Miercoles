3b670b45482f1da7d6748fb50a175e67772d9c38
andy <arteysoft@gmail.com>
2020-07-20 12:00:33 -0300

    primera parte de la clase

--- src/main/java/edu/educacionit/Init.java
+++ src/main/java/edu/educacionit/Init.java
@@ -2,13 +2,29 @@ package edu.educacionit;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class A {
+    public static int variable1;
+}
+
 @SpringBootApplication
 public class Init {
     public static void main(String[] args) {
+        /*
         Jugador compu = new JugadorConsola("juan");
         Jugador consola = new JugadorCompu();
         
         Juego juego = new Juego(compu, consola);
         juego.jugar();
+        */
+        
+        
+        
+        A a1 = new A();
+        a1.variable1 = 1000;
+        A a2 = new A();
+        a2.variable1 = 30000;
+        
+        System.out.println(a1.variable1);
+        System.out.println(a2.variable1);
     }
 }
--- src/test/java/edu/educacionit/JavaLmApplicationTests.java
+++ src/test/java/edu/educacionit/JavaLmApplicationTests.java
@@ -1,13 +1,24 @@
 package edu.educacionit;
 
+import java.util.Date;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
-@SpringBootTest
+@SpringBootTest(classes={Date.class})
 class JavaLmApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testearPiedraPapelTijeraEmpate() {
+            // A de arrange
+            Integer j1 = LogicaPPT.Piedra;
+            Integer j2 = LogicaPPT.Piedra;
+            
+            // A de act
+            Integer resultado = LogicaPPT.jugar(j1, j2);
+            
+            // A de assert
+            Assert.isTrue(resultado == LogicaPPT.Empate, "Guarda que no es empate");
 	}
-
+        
 }
